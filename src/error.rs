07524aad2d09152f6a65f925eac111f9ce881a47
Harry Hornbacher <dev@harry-hornbacher.de>
2025-07-26 22:03:51 +0200

    Add error variants for Protobuf and UUID handling

--- src/error.rs
+++ src/error.rs
@@ -10,4 +10,10 @@ pub enum Error {
     Json(#[from] serde_json::Error),
     #[error("Zip error: {0}")]
     Zip(#[from] zip::result::ZipError),
+    #[error("Protobuf decode error: {0}")]
+    ProtobufDecode(#[from] prost::DecodeError),
+    #[error("UUID parse error: {0}")]
+    UuidParse(#[from] uuid::Error),
+    #[error("UUID invalid UTF8: {0}")]
+    UuidInvalidUtf8(std::str::Utf8Error),
 }
