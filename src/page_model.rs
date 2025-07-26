0a83d61854386c2c1b398c7b0ee22134e2ac83d6
Harry Hornbacher <dev@harry-hornbacher.de>
2025-07-26 23:56:10 +0200

    Refactor PageModel and add protobuf parsing methods

--- src/page_model.rs
+++ src/page_model.rs
@@ -1,6 +1,6 @@
 use crate::{
     id::PageModelId,
-    json::{Dimensions, Layer},
+    json::{Dimensions, Layer}, utils::{convert_timestamp_to_datetime, parse_json},
 };
 
 #[derive(Debug, Clone)]
@@ -10,6 +10,18 @@ pub struct PageModel {
     pub created: chrono::DateTime<chrono::Utc>,
     pub modified: chrono::DateTime<chrono::Utc>,
     pub dimensions: Dimensions,
+}
+
+impl PageModel {
+    pub fn from_protobuf(model: &protobuf::PageModel) -> crate::error::Result<Self> {
+        Ok(Self {
+            page_model_id: PageModelId::from_str(&model.uuid)?,
+            layers: parse_json(&model.layers_json)?,
+            created: convert_timestamp_to_datetime(model.created)?,
+            modified: convert_timestamp_to_datetime(model.modified)?,
+            dimensions: parse_json(&model.dimensions_json)?,
+        })
+    }
 }
 
 mod protobuf {
@@ -27,5 +39,13 @@ mod protobuf {
         pub modified: u64,
         #[prost(string, tag = "7")]
         pub dimensions_json: String,
+    }
+
+    impl PageModel {
+        pub fn read(mut reader: impl std::io::Read) -> crate::error::Result<Self> {
+            let mut buf = Vec::new();
+            reader.read_to_end(&mut buf)?;
+            Ok(PageModel::decode(&buf[..])?)
+        }
     }
 }
