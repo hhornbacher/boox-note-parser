d0c3e27ade878eb3093b06cdf78c41b5035d398e
Harry Hornbacher <dev@harry-hornbacher.de>
2025-07-27 22:25:59 +0200

    Refactor imports and enhance virtual doc retrieval

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,21 @@ use std::collections::HashMap;
 
 use crate::{
     error::Result,
-    id::NoteUuid,
+    id::{NoteUuid, VirtualPageUuid},
     note_tree::{NoteMetadata, protobuf::NoteTree},
+    virtual_doc::VirtualDoc,
+    virtual_page::VirtualPage,
 };
 
 mod container;
 mod id;
+mod json;
 mod note_tree;
+mod page_model;
+mod shape;
 mod utils;
 mod virtual_doc;
 mod virtual_page;
-mod shape;
-mod page_model;
-mod json;
 
 pub mod error;
 
@@ -32,7 +34,9 @@ impl<R: std::io::Read + std::io::Seek> NoteFile<R> {
         let note_tree = if *container.container_type() == container::ContainerType::MultiNote {
             NoteTree::read(container.get_file("note_tree")?)?
         } else {
-            NoteTree::read(container.get_file("note/pb/note_info")?)?
+            NoteTree::read(
+                container.get_file(&format!("{}/note/pb/note_info", container.root_path()))?,
+            )?
         };
 
         for note in note_tree.notes {
@@ -51,23 +55,48 @@ impl<R: std::io::Read + std::io::Seek> NoteFile<R> {
     }
 
     pub fn list_notes(&self) -> HashMap<NoteUuid, String> {
-        self.note_metadata_map.iter().map(|(id, metadata)| (*id, metadata.name.clone())).collect()
+        self.note_metadata_map
+            .iter()
+            .map(|(id, metadata)| (*id, metadata.name.clone()))
+            .collect()
     }
 
     pub fn get_note_metadata(&self, note_id: &NoteUuid) -> Option<&NoteMetadata> {
         self.note_metadata_map.get(note_id)
     }
 
-    pub fn list_virtual_docs(&self) -> Result<Vec<virtual_doc::VirtualDoc>> {
-        let root_path = self.container.root_path();
-        todo!("Implement listing virtual docs in {}", root_path);
-        // let mut virtual_docs = Vec::new();
-        // for file in self.container.list_files()? {
-        //     if file.ends_with(".vdoc") {
-        //         let doc = virtual_doc::protobuf::VirtualDoc::read(self.container.get_file(&file)?)?;
-        //         virtual_docs.push(virtual_doc::VirtualDoc::from_protobuf(&doc)?);
-        //     }
-        // }
-        // Ok(virtual_docs)
+    pub fn get_virtual_doc(&mut self, note_id: &NoteUuid) -> Result<VirtualDoc> {
+        let note_id = note_id.to_simple_string();
+        let virtual_doc = crate::virtual_doc::protobuf::VirtualDoc::read(
+            self.container
+                .get_file(&format!("{}/virtual/doc/pb/{}", note_id, note_id))?,
+        )?;
+
+        Ok(VirtualDoc::from_protobuf(&virtual_doc)?)
+    }
+
+    pub fn get_virtual_pages(
+        &mut self,
+        note_id: &NoteUuid,
+    ) -> Result<HashMap<VirtualPageUuid, VirtualPage>> {
+        let note_id = note_id.to_simple_string();
+
+        let mut virtual_pages = HashMap::new();
+
+        for page in self
+            .container
+            .list_directory(&format!("{}/virtual/page/pb", note_id))
+        {
+            let virtual_page_id = VirtualPageUuid::from_str(&page.rsplit('/').next().unwrap())?;
+            let virtual_page_container = crate::virtual_page::protobuf::VirtualPageContainer::read(
+                self.container.get_file(&page)?,
+            )?;
+            virtual_pages.insert(
+                virtual_page_id,
+                VirtualPage::from_protobuf(&virtual_page_container.virtual_page)?,
+            );
+        }
+
+        Ok(virtual_pages)
     }
 }
